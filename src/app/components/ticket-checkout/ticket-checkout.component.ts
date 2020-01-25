4223b56353da51410c0bd4e6f7943b07ce682087
Felipe Sulczinski <matos.szk@gmail.com>
2020-01-25 18:33:26 -0300

    style(ticket-checkout): correcao nas fontes

---
+++ src/app/components/ticket-checkout/ticket-checkout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-checkout',
+  templateUrl: './ticket-checkout.component.html',
+  styleUrls: ['./ticket-checkout.component.scss']
+})
+export class TicketCheckoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
