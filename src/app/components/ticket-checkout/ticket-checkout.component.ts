9cb9dea1ce34d60a72c59a2d0d43663a6577d92d
Felipe Sulczinski <matos.szk@gmail.com>
2020-01-26 13:23:20 -0300

    feat(ticket-checkout): adicao de formbuilder

--- src/app/components/ticket-checkout/ticket-checkout.component.ts
+++ src/app/components/ticket-checkout/ticket-checkout.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-ticket-checkout',
-  templateUrl: './ticket-checkout.component.html',
-  styleUrls: ['./ticket-checkout.component.scss']
+    selector: 'app-ticket-checkout',
+    templateUrl: './ticket-checkout.component.html',
+    styleUrls: ['./ticket-checkout.component.scss']
 })
 export class TicketCheckoutComponent implements OnInit {
 
-  constructor() { }
+    checkoutForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.buildForm();
+        console.log(this.checkoutForm);
+    }
+
+    buildForm() {
+        this.checkoutForm = this.fb.group({
+            primeiroNome: this.fb.control('', [Validators.required]),
+            ultimoNome: this.fb.control('', [Validators.required]),
+            cpf: this.fb.control('', [Validators.required]),
+            dtNascimento: this.fb.control('', [Validators.required]),
+            email: this.fb.control('', [Validators.required]),
+            addAcompanhante: this.fb.control(''),
+            primeiroNomeAcompanhante: this.fb.control('', [Validators.required]),
+            ultimoNomeAcompanhante: this.fb.control('', [Validators.required]),
+            cpfAcompanhante: this.fb.control('', [Validators.required]),
+            dtNascimentoAcompanhante: this.fb.control('', [Validators.required]),
+            emailAcompanhante: this.fb.control('', [Validators.required]),
+            addAcompanhanteAcompanhante: this.fb.control(''),
+        });
+    }
 
 }
