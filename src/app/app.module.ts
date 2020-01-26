4eec10b3de65591a2a99ff57ac6452651dd82399
Felipe Sulczinski <matos.szk@gmail.com>
2020-01-26 13:22:36 -0300

    style(*): ajustes de layout fluido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { TicketCheckoutComponent } from './components/ticket-checkout/ticket-checkout.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CartComponent } from './components/cart/cart.component';
+import { CartListComponent } from './components/cart/cart-list/cart-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { CartComponent } from './components/cart/cart.component';
     HeaderComponent,
     TicketCheckoutComponent,
     FooterComponent,
-    CartComponent
+    CartComponent,
+    CartListComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        ReactiveFormsModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
